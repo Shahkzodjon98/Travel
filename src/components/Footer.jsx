487cb779c88021e5ba0317636ce3093924409707
Shahkzodjon98 <Shahkzodjon98@gmail.ru>
2023-09-15 16:36:27 +0500

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ import {AiFillInstagram} from 'react-icons/ai';
 import { FaTripadvisor } from 'react-icons/fa';
 import { AiFillYoutube } from 'react-icons/ai';
 import { FiChevronRight } from 'react-icons/fi';
+import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
 
-
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 import Aos from 'aos';
@@ -29,36 +29,64 @@ const Footer = () => {
     Aos.init({ duration: 2000 });
   }, []);
 
+
+
+  const [showArrowUp, setShowArrowUp] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowArrowUp(true);
+    } else {
+      setShowArrowUp(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <section  className='w-full relative py-8 flex items-center justify-center m-auto'>
-      <div className=' absolute h-full w-full'>
-        <video muted autoPlay loop className='object-cover absolute h-full w-full bg-gradient-to-b from-transparent via-transparent to-[rgba(10, 87, 107, 0.307)]'>
-        <source src="/video (2).mp4" type="video/mp4" />
-        </video>
+    <section  className='footer w-full relative py-8 flex items-center justify-center m-auto'>
+      <div className='secContent absolute h-full w-full'>
+
+       <video muted autoPlay loop className='object-cover absolute h-full w-full bg-gradient-to-b from-transparent via-transparent to-[rgba(10, 87, 107, 0.307)]'>
+         <source src="/video (2).mp4" type="video/mp4" />
+      </video>
+
       </div>
 
       <div className="container w-full h-max p-8 flex justify-center items-center flex-col m-auto gap-y-[2rem] z-[100]">
-        <div className=' flex justify-between flex-col gap-[1rem]'>
+        <div className='secContent flex justify-between flex-col gap-[1rem]'>
           <div data-aos="fade-up" className=' p-4 '>
             <small className=' text-[14px] font-normal text-[#fff]'>KEEP IN TOUCH</small>
             <h2 className=' text-[2.0rem] font-extrabold text-[#fff]'>Travel with us</h2>
           </div>
 
 
-          <div className="flex w-full gap-[1rem] flex-col">
+          <div className="footerCard flex w-full gap-[1rem] flex-col">
           <input  data-aos="fade-up" type="text" placeholder="Enter Email Address" class="w-full px-6 py-2 rounded-full outline-none bg-transparent border-2 border-white placeholder-white placeholder-opacity-150 text-white"/>
             <button  data-aos="fade-up" className='btn flex w-full justify-center text-white font-semibold gap-[.5rem]' type='submit'>SEND<FiSend className='text-[18px]'/></button>
           </div>
         </div>
 
 
-        <div className=" relative flex w-full rounded-[1rem] gap-[2rem] p-4 pb-16 bg-[#f5f1f1] flex-col justify-center items-start hidden">
-          <div className='flex basis-[50%] justify-center items-start flex-col gap-y-[.5rem]'>
+        <div className="footerLinks relative flex w-full rounded-[1rem] gap-[2rem] p-4 pb-16 bg-[#f5f1f1] flex-col justify-center items-start hidden">
+          <div className='footerLinks flex basis-[50%] justify-center items-start flex-col gap-y-[.5rem]'>
             <div className="logoDiv">
               <a href="#" className=' flex text-black font-semibold	'> <MdOutlineTravelExplore className='text-[25px] mr-[10px] text-[#2483acbb]'/> Travel.</a>
             </div>
 
-            <div  data-aos="fade-up" className=" text-[13px] text-[#5b5864]">
+            <div  data-aos="fade-up" className="footerLinks text-[13px] text-[#5b5864]">
               Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Большой рукопись, наш переписывается семантика lorem взгляд всемогущая текстов страна пустился жизни за составитель жаренные о! Толку щеке курсивных, она за гор пустился продолжил все маленький, силуэт они даже дорогу использовало великий заголовок власти на берегу свой запятой, ему большого решила. 
             </div>
 
@@ -71,7 +99,7 @@ const Footer = () => {
           </div>
 
 
-          <div  className="grid grid-cols-2 basis-[50%] w-full gap-[1rem] justify-between">
+          <div  className="footerLinks grid grid-cols-2 basis-[50%] w-full gap-[1rem] justify-between">
         {/* Group one  */}
             <div  data-aos="fade-up" data-aos-duration="3000"className="linkGroup">
               <span className='block font-semibold text-black mb-[.5rem]'>
@@ -174,6 +202,20 @@ const Footer = () => {
             <div className='flex flex-col absolute bottom-0  left-0 right-0 bg-[#2483acbb] py-3 px-8 rounded-b-2xl'>
             <small className='text-[10px] text-white'>BEST TRAVEL WEBSITE THEME</small>
             <small className='text-[10px] text-white'>COPYRIGHTS RESERVED - Shakhzod_5544 2023</small>
+
+       
+      {/* Conditionally render the arrow-up button */}
+      {showArrowUp && (
+        <div
+          className="w-12 h-12 border border-black/100 rounded-full flex-center cursor-pointer absolute right-1 bottom-16 xl:bottom-8 xl:-right-3 bg-[#2483acbb] transition-200 hover:border-white hover:bg-white group desktop-top"
+          data-v-3cb8761c=""
+          onClick={scrollToTop}
+        >
+          <BsFillArrowUpCircleFill className="flex justify-center items-center  text-[48px]" />
+        </div>
+      )}
+
+
             </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,56 +21,54 @@ const Navbar = () => {
 
 
   return (
-    <section className='navBarSection'>
-      <header className=" fixed flex justify-between items-center bg-[#f0efef] w-full z-[1000] p-[1rem]  shadow shadow-blue-900/50 hover:shadow-indigo-900/50">
+   <section className='navBarSection'>
+  <header className="header-bg fixed flex justify-between items-center w-full z-[1000] p-[1rem] shadow shadow-blue-900/50 hover:shadow-indigo-900/50">
 
-          <div className="logo text-[#362020] ">
-            <a href="#" className=" flex font-semibold items-center justify-center cursor-pointer">
-              <h1 className=' text-4xl text-blue-500 flex p-2'><MdTravelExplore className='icon-1 text-[25px] text-[#3c8dc6] translate-y-[5px]'/>Travel.</h1>
-            </a>
-          </div>
+    <div className="logo text-[#362020] ">
+      <a href="#" className="flex font-semibold items-center justify-center cursor-pointer">
+        <h1 className='text-4xl text-blue-500 flex p-2'><MdTravelExplore className='icon-1 text-[25px] text-[#3c8dc6] translate-y-[5px]' />Travel.</h1>
+      </a>
+    </div>
 
-          <div className={`${active} max-md:absolute bg-[#f0efef] h-max w-[80%] rounded-[1rem] top-[-500rem] left-[50%] z-[2000] bg-black-100 shadow-lg shadow-cyan-500/50 translate-x-[-50%] transition duration-100 ease-in-out p-[1rem]`}>
-                <ul className=" flex flex-col justify-center w-full m-auto py-2 px-0">
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Home</a>
-                  </li>
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Packages</a>
-                  </li>
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Shop</a>
-                  </li>
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>About</a>
-                  </li>
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Pages</a>
-                  </li>
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>News</a>
-                  </li>
-                  <li className=' py-2 px-0'>
-                  <a href="#" className=' text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Contact</a>
-                  </li>
+    <div className={`lg:${active} navBar max-md:absolute bg-[#f0efef] h-max w-[80%] lg:w-auto rounded-[1rem] top-[-500rem] left-[50%] z-[2000] bg-black-100 shadow-lg shadow-cyan-500/50 lg:shadow-none translate-x-[-50%] lg:translate-x-0 transition duration-100 ease-in-out p-[1rem]`}>
+      <ul className="navLists flex flex-col lg:flex-row justify-center w-full m-auto py-2 px-0 lg:py-0">
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Home</a>
+        </li>
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Packages</a>
+        </li>
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Shop</a>
+        </li>
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>About</a>
+        </li>
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Pages</a>
+        </li>
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>News</a>
+        </li>
+        <li className='py-2 px-0 lg:px-4'>
+          <a href="#" className='text-[#706e6e] hover:text-[#3c8dc6] text-2xl font-semibold'>Contact</a>
+        </li>
+        <button className='btn mt-[1rem] lg:mt-0 lg:ml-4'>
+          <a href="#" className='font-semibold text-[#f0efef]'>Book Now</a>
+        </button>
+      </ul>
 
-                  <button className='btn mt-[1rem]'>
-                    <a href="#" className='font-semibold text-[#f0efef]'>Book Now</a>
-                  </button>
-                </ul>
+      <div onClick={removeNavbar} className="closeNavbar lg:hidden absolute top-[1rem] right-[1.5rem] text-[#3c8dc6] hover:text-[#362020]">
+        <AiFillCloseCircle className='icon-2 text-[25px] text-[#3c8dc6] hover:text-[#706e6e]' />
+      </div>
+    </div>
 
+    <div onClick={showNav} className="toggleNavbar lg:hidden">
+      <PiDotsNineBold className='icon-3 text-[25px] text-[#3c8dc6] hover:text-[#706e6e] ' />
+    </div>
+  </header>
+</section>
 
-                <div onClick={removeNavbar} className=" absolute top-[1rem] right-[1.5rem] text-[#3c8dc6] hover:text-[#362020]">
-                  <AiFillCloseCircle className='icon-2 text-[25px] text-[#3c8dc6] hover:text-[#706e6e]'/>
-                </div>
-              </div>  
-
-
-              <div onClick={showNav} className="toggleNavbar ">
-                <PiDotsNineBold className='icon-3 text-[25px] text-[#3c8dc6] hover:text-[#706e6e] '/>
-              </div>
-      </header>
-    </section>
   )
 }
 
